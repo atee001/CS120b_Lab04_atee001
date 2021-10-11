1dbeda342869fdb25f9763ab490106e0d2191b6e
Andrew Tee <atee001@wch136-24.cs.ucr.edu>
2021-10-10 17:45:24 -0700

    Initial commit

--- source/main.c
+++ source/main.c
@@ -7,17 +7,78 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
+#include <stdio.h>
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
+enum LED_State { Start, Init, Press_off, Release_off, Press_on } LED_State;
+
+void Tick()
+{
+  switch(LED_State) {   // Transitions
+     case Start:  // Initial transition
+        LED_State = Init;
+        break;
+
+     case Init:
+        LED_State = (PINA & 0x01) ? Press_off : Init;
+	break;
+
+     case Press_off:
+	LED_State = !(PINA & 0x01) ? Release_off : Press_off;
+        break;
+
+     case Release_off:
+	LED_State = (PINA & 0x01) ? Press_on : Release_off;
+	break;
+     
+     case Press_on:
+	LED_State = !(PINA & 0x01) ? Init : Press_on;	
+	break;
+
+     default:
+        printf("Error!\n");
+        break;
+  } // Transitions
+
+  switch(LED_State) {   // Transitions
+     case Start:  // Initial transition
+        break;
+
+     case Init:
+        PORTB = 0x01;
+        break;
+
+     case Press_off:
+	PORTB = 0x02;
+        break;
+
+     case Release_off:
+	PORTB = 0x02;
+        break;
+
+     case Press_on:
+	PORTB = 0x01;
+        break;
+
+     default:
+        printf("Error!\n");
+        break; 
+
+  } // State actions
+}
+
 int main(void) {
-    /* Insert DDR and PORT initializations */
+   DDRB = 0xFF; PORTB = 0x00;
+   DDRA = 0x00; PORTA = 0xFF;      
+        // Initialize outputs
+   LED_State = Start; // Indicates initial call
 
-    /* Insert your solution below */
-    while (1) {
+   while(1) {
+      Tick();
+   }
 
-    }
-    return 1;
+  return 1;
 }
