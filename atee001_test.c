498b59bd555810c5ef910885d8c25a6bbddc68ad
atee001 <80326381+atee001@users.noreply.github.com>
2021-10-24 16:23:34 -0700

    Update atee001_test.c

--- atee001_test.c
+++ atee001_test.c
@@ -1,5 +1,14 @@
-#include <avr/io.h>
+/*	Author: lab
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
 
+#include <avr/io.h>
 #include <avr/interrupt.h>
 #ifdef SIMULATE
 #include "simAVRHeader.h"
@@ -58,4 +67,79 @@ ISR(TIMER1_COMPA_vect) {
 void TimerSet (unsigned long M) {
     _avr_timer_M = M;
     _avr_timer_cntcurr = _avr_timer_M;
+}
+
+enum LED_States {init, LED_One, LED_Two, LED_Three} Led_State;
+
+void Tick(){
+
+	switch(LED_State){
+
+
+		case init:
+
+			LED_State = init;
+			break;
+
+		case LED_One:
+	
+			LED_State = LED_Two;
+			break;
+
+		case LED_Two:
+
+			LED_State = LED_Three
+			break;
+
+		case LED_Three:
+			
+			LED_State = LED_One;
+			break;
+
+		default: 
+			break;
+
+
+	}
+
+
+	switch(LED_State){
+
+		case LED_One:
+			
+			PORTB = 0x01;
+			break;
+		
+		case LED_Two:
+
+			PORTB = 0x02;
+			break;
+
+		case LED_Three:
+
+			PORTB = 0x04;
+			break;
+
+		default: 
+			break;
+
+	}
+
+
+}
+void main(){
+
+	TimerSet(1000);
+	TimerOn();
+	LED_State = init;
+	while(1){
+		Tick();
+		while(!TimerFlag){}
+		TimerFlag = 0;
+
+	}
+
+
+
+
 }
